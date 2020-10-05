33c6f2d59478a102c80638cf71ac7aecd6766850
Felipe Moreira <felipemoreira.97@hotmail.com>
2020-10-05 06:08:11 -0300

    Inserido em forma de charts as críticas (%) disponíveis em cada filme/série

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -6,13 +6,20 @@ import EventIcon from "@material-ui/icons/Event";
 import MovieFilterIcon from "@material-ui/icons/MovieFilter";
 import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
 import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
-import "./styles.css";
-
 import LoadingDetails from "../LoadingDetails";
+
+import rt from "../../images/PNG/rotten.png";
+import meta from "../../images/PNG/metacritic.png";
+import imdb from "../../images/PNG/imdb.png";
+
+import { PieChart } from "react-minimal-pie-chart";
+
+import "./styles.css";
 
 const Details = (props) => {
   const [details, setDetails] = useState({});
   const [loading, setLoading] = useState(false);
+  const [dados, setDados] = useState([]);
 
   const separateActors = (act) => {
     for (let i = 0; i < act.length; i++) {
@@ -31,6 +38,30 @@ const Details = (props) => {
     return year !== undefined && year.length === 5 ? year.substr(0, 4) : year;
   };
 
+  const insertCharts = (dados) => {
+    let colors = ["#e0ab00", "#fa3306", "#022330"];
+    let imgs = [imdb, rt, meta];
+    let dt = dados.map((v, index) => {
+      return (
+        <div key={index}>
+          <p style={{ color: colors[index] }}>{v.val}%</p>
+          <div>
+            <img src={imgs[index]} alt={v.nome} />
+            <PieChart
+              className="chart-critic"
+              data={[{ value: v.val, color: colors[index] }]}
+              totalValue={100}
+              lineWidth={20}
+              rounded
+            />
+          </div>
+        </div>
+      );
+    });
+
+    return dt;
+  };
+
   useEffect(() => {
     setLoading(true);
     const loadDetails = async () => {
@@ -38,6 +69,16 @@ const Details = (props) => {
       setLoading(false);
       separateActors(response.data.Actors.split(", "));
       setDetails(response.data);
+
+      const dados = [];
+
+      response.data.Ratings.forEach((e) => {
+        dados.push({
+          nome: e.Source === "Internet Movie Database" ? "IMDB" : e.Source,
+          val: parseInt(e.Value.substr(0, 3).replace(/\./g, "")),
+        });
+      });
+      setDados(dados);
     };
 
     loadDetails();
@@ -90,9 +131,13 @@ const Details = (props) => {
               </div>
             </div>
           </div>
+          {dados.length > 0 && (
+            <div className="chart-review" id="chart-review">
+              {insertCharts(dados)}
+            </div>
+          )}
         </>
       )}
-
       {loading && <LoadingDetails />}
     </div>
   );
